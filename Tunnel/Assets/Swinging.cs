fc3006cac655ba0b53cbaca7e157c3890b988779
Jon Manning <jon@secretlab.com.au>
2014-05-26 13:42:11 +1000

    Use accelerometer to control the gnome.

--- Tunnel/Assets/Swinging.cs
+++ Tunnel/Assets/Swinging.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Require a 2D physicsbody
+[RequireComponent (typeof(Rigidbody2D))]
 public class Swinging : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float swingSensitivity = 1.0f;
+
+	void FixedUpdate() {
+		// get the swing amount
+
+		float swing = InputManager.instance.sidewaysMotion;
+
+		// calculate a force
+
+		Vector2 force = new Vector2(swing * swingSensitivity, 0);
+
+		// apply the force to the rigidbody
+
+		rigidbody2D.AddForce(force);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
