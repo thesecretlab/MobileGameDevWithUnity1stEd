976d1db8db0944700673eea15f50e817250ef212
Jon Manning <jon@secretlab.com.au>
2014-05-26 13:31:05 +1000

    Make InputManager a singleton, and make it expose sideways tilt as a property. Remove some unneeded files and amend gitignore.

--- Tunnel/Assets/InputManager.cs
+++ Tunnel/Assets/InputManager.cs
@@ -3,6 +3,26 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour {
 
+	private static InputManager _instance;
+
+	public static InputManager instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<InputManager>();
+			}
+			return _instance;
+		}
+	}
+
+	// -1.0 = full left, +1.0 = full right
+	private float _sidewaysMotion = 0.0f;
+
+	public float sidewaysMotion {
+		get {
+			return _sidewaysMotion;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +32,7 @@ public class InputManager : MonoBehaviour {
 	void Update () {
 		Vector3 accel = Input.acceleration;
 
-		Debug.Log(accel);
+		_sidewaysMotion = accel.x;
+
 	}
 }
