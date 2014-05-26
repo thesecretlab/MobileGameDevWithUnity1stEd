6af42794c418d1f9971259e12bf9e44aafe03417
Jon Manning <jon@secretlab.com.au>
2014-05-26 13:16:51 +1000

    Log acceleration events

---
+++ Tunnel/Assets/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 accel = Input.acceleration;
+
+		Debug.Log(accel);
+	}
+}
