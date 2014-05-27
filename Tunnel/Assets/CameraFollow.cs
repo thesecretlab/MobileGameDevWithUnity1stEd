842dcef682c1ce6712d4a03c885d79c82b23e8a6
Jon Manning <jon@secretlab.com.au>
2014-05-27 14:32:34 +1000

    Camera now follows the gnome

---
+++ Tunnel/Assets/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+	public float topLimit;
+	public float bottomLimit;
+
+	void LateUpdate () {
+
+		Vector3 newPosition = this.transform.position;
+
+		newPosition.y = Mathf.Lerp (newPosition.y, target.transform.position.y, 0.5f);
+		newPosition.y = Mathf.Min(newPosition.y, topLimit);
+		newPosition.y = Mathf.Max(newPosition.y, bottomLimit);
+		transform.position = newPosition;
+
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+
+		Vector3 topPoint = new Vector3(this.transform.position.x, topLimit, this.transform.position.z);
+		Vector3 bottomPoint = new Vector3(this.transform.position.x, bottomLimit, this.transform.position.z);
+
+		Gizmos.DrawLine(topPoint, bottomPoint);
+	}
+}
