76d8d20a7a9355cba5c06dc29e5b892f6ba3420a
Jon Manning <jon@secretlab.com.au>
2014-05-27 15:40:31 +1000

    Treasure is now collectable

---
+++ Tunnel/Assets/Treasure.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Treasure : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.CompareTag("Player")) {
+			// remove this object
+			gameObject.SetActive(false);
+		}
+	}
+}
