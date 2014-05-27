df02d407dc6f59d166e92a104b6aad7adade71f4
Jon Manning <jon@secretlab.com.au>
2014-05-27 10:18:22 +1000

    Add code for responding to button clicks

---
+++ Tunnel/Assets/ButtonClicking.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClicking : MonoBehaviour {
+
+	public GameObject target;
+
+	public string mouseDownMessage;
+	public string mouseUpMessage;
+
+	void OnMouseDown() {
+		if (mouseDownMessage != null)
+			target.SendMessage(mouseDownMessage, SendMessageOptions.RequireReceiver);
+	}
+
+	void OnMouseUp() {
+		if (mouseDownMessage != null)
+			target.SendMessage(mouseDownMessage, SendMessageOptions.RequireReceiver);
+	}
+}
