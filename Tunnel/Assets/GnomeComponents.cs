e59c509b8a4b1c26ddca2ea327e9128466de9b15
Jon Manning <jon@secretlab.com.au>
2014-05-28 20:25:25 +1000

    Some refactoring. Make the gnome shatter on death.

---
+++ Tunnel/Assets/GnomeComponents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GnomeComponents : MonoBehaviour {
+
+	public Rigidbody2D ropeBody;
+	public GameObject treasure;
+
+	public void DestroyGnome() {
+		// find all child objects, and disconnect their joints
+
+		foreach (Transform part in transform) {
+
+			foreach (Joint2D joint in part.GetComponentsInChildren<Joint2D>()) {
+				Destroy (joint);
+			}
+
+		}
+	}
+}
