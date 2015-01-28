aa251fd6b098326fff93b17aa222709c17fc15e2
Jon Manning <jon@secretlab.com.au>
2015-01-28 13:16:39 +1100

    Gnome eyes now rotate [delivers #87060844]

---
+++ Tunnel/Assets/EyeLookDown.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeLookDown : MonoBehaviour {
+
+	public float rotation = -90.0f;
+
+	void LateUpdate() {
+		transform.rotation = Quaternion.Euler(0, 0, rotation);
+	}
+}
