eb91d38d9cc69e0eef897ec5d4e54a233b972fb0
Jon Manning <jon@secretlab.com.au>
2015-01-28 21:18:05 +1100

    Add main menu. [Delivers #87083676]

---
+++ Tunnel/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	AsyncOperation sceneLoadingOperation;
+
+	bool loadSceneWhenReady = false;
+
+	public RectTransform loadingOverlay;
+
+	public void Start() {
+
+		loadingOverlay.gameObject.SetActive(false);
+
+		StartCoroutine("LoadSceneInBackground");
+	}
+
+	public void Update() {
+		if (sceneLoadingOperation != null) {
+
+			if (loadSceneWhenReady) {
+				sceneLoadingOperation.allowSceneActivation = true;
+			}
+
+		}
+	}
+
+	IEnumerator LoadSceneInBackground() {
+
+		sceneLoadingOperation = Application.LoadLevelAsync(sceneToLoad);
+		sceneLoadingOperation.allowSceneActivation = false;
+		yield return sceneLoadingOperation;
+	}
+
+	public void LoadScene() {
+
+		loadingOverlay.gameObject.SetActive(true);
+
+		loadSceneWhenReady = true;
+
+	}
+}
