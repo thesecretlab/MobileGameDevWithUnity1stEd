11de8041f5ccc79bd815078958013b79294ab9cf
Jonathon Manning <jon@secretlab.com.au>
2016-04-14 10:27:07 +1000

    Started work on editor extension example project

---
+++ Editor Extensions/Assets/Editor/TextureCounter.cs
@@ -0,0 +1,27 @@
+﻿// BEGIN editor_include
+using UnityEngine;
+using System.Collections;
+// BEGIN editor_include_highlight
+using UnityEditor;
+// END editor_include_highlight
+// END editor_include
+
+public class TextureCounter : EditorWindow {
+
+	[MenuItem("Window/Texture Counter")]
+	public static void Init() {
+		var window = EditorWindow.GetWindow<TextureCounter>();
+
+		// Stops this window from being unloaded when a 
+		// new scene is loaded
+		DontDestroyOnLoad(window);
+	}
+
+	private void OnGUI() {
+
+		EditorGUILayout.BeginVertical();
+		GUILayout.Button("what up");
+		EditorGUILayout.EndVertical();
+	}
+
+}
